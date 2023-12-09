f44e1f4e5f2f0513377aaf665735b390de7cc611
Felipy Penha Botega <51219829+FelipyPenhaBotega@users.noreply.github.com>
2023-12-08 22:56:18 -0300

    corrige comentários

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -2,15 +2,15 @@ import pytest
 from app.app import app
 from flask import Flask, abort
 
-# Defina uma fixture para criar um cliente de teste para o aplicativo Flask
+# Define uma fixture para criar um cliente de teste para o aplicativo Flask
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
     return app.test_client()
 
-# Escreva testes usando o client
+# Escreve testes usando o client
 def test_process_wine_data(client):
-    # Envie uma solicitação POST simulando dados do formulário
+    # Envia uma solicitação POST simulando dados do formulário
     response = client.post("/vinho", data={
         "type": "white",
         "fixed_acidity": "7.1",
@@ -26,8 +26,5 @@ def test_process_wine_data(client):
         "alcohol": "10.3",
     })
     
-    # Verifique se a resposta tem um código de status 200 (OK)
+    # Verifica se a resposta tem um código de status 200 (OK)
     assert response.status_code == 200
-
-    # Adicione mais asserções conforme necessário para testar o resultado
-    # Exemplo: assert response.get_json()["quality"] == esperado_qualidade
